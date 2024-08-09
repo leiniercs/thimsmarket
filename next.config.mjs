b48e18280f9753bcef784d1475d6b02833911114
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-08-09 16:55:18 +0400

    Minor change to CSP

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const cspHeader = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https://statichunt.com https://statichunt-images.netlify.app;
+    img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
