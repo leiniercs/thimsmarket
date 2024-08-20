0bbbbe40e696534c27cf2301a3591e89837b49d3
Leinier Cruz Salfran <leiniercs@gmail.com>
2024-08-20 08:46:30 +0400

    Minor fix

--- components/common/database.ts
+++ components/common/database.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-export let dbClient: MongoClient = new MongoClient(
-   process.env.DB_URI as string,
-   { appName: "Thims Market - API Server", minPoolSize: 25 }
-);
+export let dbClient: MongoClient = new MongoClient(process.env.DB_URI, {
+   appName: "Thims Market - API Server",
+   minPoolSize: 25
+});
